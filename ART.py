c5f49a3cb957cd96a11d7e593ca7a8836428af97
Simon Lee <simonlee@me.com>
2024-05-31 12:49:22 +0800

    Updated: 2024-05-31 12:49 PM

--- ART.py
+++ ART.py
@@ -8,5 +8,13 @@ import streamlit as st
 z='ART'
 st.set_page_config(page_title=z)
 st.title(z)
+
 if ul.stCheckPW('password_CR'):
-  ql.runART(isSkipTitle=True)
+  l = ul.spl('SPY,QQQ,TLT,GLD')
+  cols = st.columns(len(l))
+  checkboxes = [cols[i].checkbox(l[i], value=True) for i in range(len(l))]
+  multE=checkboxes[0]*1
+  multQ=checkboxes[1]*1
+  multB=checkboxes[2]*1
+  multG=checkboxes[3]*1
+  ql.runART(multE=multE,multQ=multQ,multB=multB,multG=multG,isSkipTitle=True)
