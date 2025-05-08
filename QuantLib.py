19965a3cf2fb39ac7e1762451dec651b10247864
Simon Lee <simonlee@me.com>
2025-05-09 07:58:43 +0800

    Updated: 2025-05-09 07:58 AM

--- QuantLib.py
+++ QuantLib.py
@@ -9,6 +9,7 @@ import requests
 import math
 import pendulum
 import yahooquery
+import curl_cffi
 import warnings
 from sklearn.linear_model import LinearRegression
 
@@ -282,11 +283,22 @@ def getStateS(isEntryS, isExitS, isCleaned=False, isMonthlyRebal=True):
     stateS=cleanS(stateS, isMonthlyRebal=isMonthlyRebal)
   return stateS.astype(float)
 
+'''
 def getYClose2Y(ticker):
   proxies = {'http': st.secrets['proxy'], 'https': st.secrets['proxy']}
   with warnings.catch_warnings():
     warnings.simplefilter('ignore', category=FutureWarning)
     df = yahooquery.Ticker(ticker,proxies=proxies).history(period='2y')
+  df.index = df.index.droplevel('symbol')
+  df.index = pd.to_datetime(df.index.map(lambda x: pendulum.parse(str(x)).date()))
+  return df['adjclose'].rename(ticker)
+'''
+def getYClose2Y(ticker):
+  #proxies = {'http': st.secrets['proxy'], 'https': st.secrets['proxy']}
+  with warnings.catch_warnings():
+    warnings.simplefilter('ignore', category=FutureWarning)
+    session = curl_cffi.Session(impersonate="chrome")
+    df = yahooquery.Ticker(ticker,session=session).history(period='2y')
   df.index = df.index.droplevel('symbol')
   df.index = pd.to_datetime(df.index.map(lambda x: pendulum.parse(str(x)).date()))
   return df['adjclose'].rename(ticker)
